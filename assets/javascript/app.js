55e28c4934e2705ca6976bf00f098eec6c5be38d
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-06-23 19:17:16 -0600

    added a little js

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+var qualities = ["Web Developer", "Coffee Enthusiast", "Aspiring Nomad"];
+
+function create(){
+    for(var i = 0; i <qualities.length; i++){
+    var newDiv = $("<h2>")
+    newDiv.append(qualities[i]);
+    $(".lead").append(newDiv);
+    }
+ 
+}
+
+setTimeout(function() {
+    create();
+}, 1000, setTimeout(function(){
+    $(".heading").hide();
+    $(".welcome").show();
+}, 4000));
